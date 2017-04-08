6881bfb5e8150e21a5801a4b3eb22e0aa8904410
hellomephllw <hellomephllw@163.com>
2017-04-08 11:46:10 +0800

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,22 @@
 /**
  * Created by liliwen on 2017/4/8.
  */
+'use strict';
+const webpack = require('webpack');
+
+const config = {
+    entry: {
+
+    },
+    output: {
+
+    },
+    module: {
+
+    },
+    plugins: {
+
+    }
+};
+
+module.export = config;
